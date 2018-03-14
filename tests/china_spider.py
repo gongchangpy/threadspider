fdda637c45ed15062b7863153131190530a1c708
zhoukunpeng504 <18749679769@163.com>
2018-03-14 14:35:41 +0800

    spider优化

--- tests/china_spider.py
+++ tests/china_spider.py
@@ -10,15 +10,18 @@ import re
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+# headers伪装
 headers = {}
 headers["User-Agent"] = 'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html'
 
+# 爬虫线程池初始化
 spider_init(20)
 
 mongo_client = pymongo.MongoClient("192.168.8.137", 27017)
 db = mongo_client["zkptest"]
 
 
+# 首页及相关聚合页处理函数
 def res_handle(res):
     doc = pyquery.PyQuery(res)
     for i in doc("a"):
@@ -27,13 +30,13 @@ def res_handle(res):
             if re.search(r"site.china.cn/[a-z]+/\d+\.html.*?", href):
                 href = href.split("?")[0]
                 Spider(href, headers=headers, response_handle=get_detail_handle(href), retry_times=3, charset="gbk",
-                       priority=3)
+                       priority=3)  # 爬取详情
             else:
 
                 href = href.split("?")[0]
-                Spider(href, headers=headers, response_handle=res_handle, retry_times=3, charset="gbk")
+                Spider(href, headers=headers, response_handle=res_handle, retry_times=3, charset="gbk") # 爬取聚合页
 
-
+# 详情页处理函数
 def get_detail_handle(url):
     def detail_handle(res):
         try:
@@ -57,5 +60,5 @@ def get_detail_handle(url):
     return detail_handle
 
 
-Spider("https://site.china.cn/", response_handle=res_handle, headers=req, retry_times=3, charset="gbk")
-spider_join()
+Spider("https://site.china.cn/", response_handle=res_handle, headers=headers, retry_times=3, charset="gbk")  # 爬虫入口页面
+spider_join()  # 等待爬虫执行完成
